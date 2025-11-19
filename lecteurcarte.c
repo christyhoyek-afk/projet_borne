d67a841b1b1e27738bf7af17e7e42a052434b644
Christian HOYEK <christ.y.hoyek@gmail.com>
2025-11-19 09:43:56 +0100

    prems

--- lecteurcarte.c
+++ lecteurcarte.c
@@ -1,16 +1,25 @@
 #include <unistd.h>
 #include "lecteurcarte.h"
 #include <stdio.h>
-#include "lcarte.h"
+#include <lcarte.h>
+#include <lvoyant.h>
 
 void lecteurcarte_initialiser()
 {
-
+    initialisations_ports();
 }
 
 void lecteurcarte_lire_carte()
 {
     attente_insertion_carte();
-    printf("Votre numéro de carte : %d\n", lecture_numero_carte());
+    printf("Carte insérée ?.\n");
+    numero= lecture_numero_carte();
+    printf("Votre numéro de carte : %d\n", numero);
+
+    if(carte_insere && voyant_dispo){
+        int authentifier= base_client_authentifier(numero);
+        printf("Authentification %d\n", authentifier ? "réussie" : "échouée");
+
+    }
 }
 
